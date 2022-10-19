d964b4c7eba360114cde51560c522370e84000ed
digitalworks_hgpark <phk9436@naver.com>
2022-10-19 11:01:06 +0900

    [Feat] to do list 입력폼 작성
    - useState를 이용한 입력값 관리
    
    -map함수를 이용해 입력값 리스트로 append시키기
    
    - eslintrc.json 세팅

--- src/App.js
+++ src/App.js
@@ -1,50 +1,33 @@
-import styles from "./App.module.css";
-import { useState, useEffect } from "react";
+import React, { useState } from 'react';
 
-function Hello() {
-  useEffect(() => {
-    console.log("created");
-    return () => console.log("destryed");
-  }, []);
-  return <h1>Hello</h1>;
-}
-
-function App() {
-  const [Counter, setCounter] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const [showing, setshowing] = useState(false);
-
-  const onClick = () => setCounter(Counter + 1);
-  const onChange = (e) => setKeyword(e.target.value);
-  const onShow = () => setshowing(!showing);
-
-  useEffect(() => console.log("render"), []);
-  useEffect(() => {
-    if (keyword !== "" && keyword.length > 5) console.log("Search API");
-  }, [keyword]);
-  useEffect(() => {
-    if (Counter > 0) console.log("Counting");
-  }, [Counter]);
-  useEffect(() => {
-    if (keyword !== "" || Counter > 0) console.log("both changing");
-  }, [keyword, Counter]);
+function App () {
+  const [toDo, settoDo] = useState('');
+  const [toDoLi, settoDoLi] = useState([]);
+  const onChange = (e) => settoDo(e.target.value);
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (toDo === '') {
+      return false;
+    } else {
+      settoDo('');
+      settoDoLi((array) => [toDo, ...array]);
+    }
+  };
 
   return (
     <div className="App">
-      <h1 className={styles.title}>React</h1>
-      <p>
-        {Counter} {keyword}
-      </p>
-      <input
-        type="text"
-        placeholder="Search here"
-        onChange={onChange}
-        value={keyword}
-      />
-      <button onClick={onClick}>Counter</button>
-
-      <button onClick={onShow}>{showing ? "Hide" : "Show"}</button>
-      {showing ? <Hello /> : null}
+      <h1>My To Dos ({toDoLi.length})</h1>
+      <ul>
+        {toDoLi.map((e, i) => {
+          return (
+            <li key={i}>{e}</li>
+          );
+        })}
+      </ul>
+      <form action="" onSubmit={onSubmit}>
+        <input type="text" placeholder='Write to do' value={toDo} onChange={onChange}/>
+        <button>Add to do</button>
+      </form>
     </div>
   );
 }
