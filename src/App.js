7159473692b01ee77a887a8833eeb7c4f05b83fd
digitalworks_hgpark <phk9436@naver.com>
2022-10-19 10:54:00 +0900

    [Feat] useEffect 실습
    - useEffect는 렌더링 이후와 디펜던시가 변경될때마다 실행된다.
    
    - useEffect의 디펜던시를 빈 배열로 넣으면 렌더링 이후 단 한 번만 실행된다.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
-import Button from "./Button";
 import styles from "./App.module.css";
+import { useState, useEffect } from "react";
+
+function Hello() {
+  useEffect(() => {
+    console.log("created");
+    return () => console.log("destryed");
+  }, []);
+  return <h1>Hello</h1>;
+}
 
 function App() {
+  const [Counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const [showing, setshowing] = useState(false);
+
+  const onClick = () => setCounter(Counter + 1);
+  const onChange = (e) => setKeyword(e.target.value);
+  const onShow = () => setshowing(!showing);
+
+  useEffect(() => console.log("render"), []);
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 5) console.log("Search API");
+  }, [keyword]);
+  useEffect(() => {
+    if (Counter > 0) console.log("Counting");
+  }, [Counter]);
+  useEffect(() => {
+    if (keyword !== "" || Counter > 0) console.log("both changing");
+  }, [keyword, Counter]);
+
   return (
     <div className="App">
       <h1 className={styles.title}>React</h1>
-      <Button text={"버튼"}></Button>
+      <p>
+        {Counter} {keyword}
+      </p>
+      <input
+        type="text"
+        placeholder="Search here"
+        onChange={onChange}
+        value={keyword}
+      />
+      <button onClick={onClick}>Counter</button>
+
+      <button onClick={onShow}>{showing ? "Hide" : "Show"}</button>
+      {showing ? <Hello /> : null}
     </div>
   );
 }
