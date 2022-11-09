1269d44e89434ed6df70dde2a85ca72607c5f182
digitalworks_hgpark <phk9436@naver.com>
2022-11-09 09:08:13 +0900

    [Feat] 다이나믹 임포트 적용

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './Routes/Home';
-import Detail from './Routes/Detail';
+import loadable from '@loadable/component';
+
+const Home = loadable(() => import('./Routes/Home'));
+const Detail = loadable(() => import('./Routes/Detail'));
 
 function App () {
   return (
     <BrowserRouter>
       <Routes>
-          <Route path={`${process.env.PUBLIC_URL}/`} element={<Home/>}/>
-          <Route path='/movie/:id' element={<Detail/>}/>
+        <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />} />
+        <Route path="/movie/:id" element={<Detail />} />
       </Routes>
     </BrowserRouter>
   );
